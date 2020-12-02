cb049f227102351d5416ce5dddfec58a954e5a16
fabianhaef <fabian.haefi@gmail.com>
2020-12-02 20:53:49 +0100

    master

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -23,9 +23,7 @@ const userSchema = new mongoose.Schema({
     password: String
 })
 
-const secret = process.env.SECRET
-
-userSchema.plugin(encrypt, { secret: secret, encryptedFields=['password'] })
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields=['password'] })
 
 const User = new mongoose.model("User", userSchema)
 
